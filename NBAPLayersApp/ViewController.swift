4c59ace2d4275ed4ed2167b842629f19ce945884
snowty-developer <snowty.player@gmail.com>
2020-12-24 23:02:37 +0300

    Initial Commit

---
+++ NBAPLayersApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NBAPLayersApp
+//
+//  Created by Александр Зубарев on 24.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
